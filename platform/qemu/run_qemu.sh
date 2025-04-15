343b0314be8ac7dfcb93b615f8ced69721be4d28
scarletshroud <ezinovichev@gmail.com>
2025-04-16 00:08:31 +0300

    add qemu run script

---
+++ platform/qemu/run_qemu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Configuration
+QEMU=qemu-system-riscv32
+MACHINE=virt        # или "virt" — зависит от цели
+KERNEL=build/example.elf
+GRAPHIC="-nographic"     # отключить графику, вывод в терминал
+DEBUG_PORT=1234
+
+# Check Elf-file
+if [ ! -f "$KERNEL" ]; then
+  echo "Image $KERNEL doesn't exists. Build project with CMake."
+  exit 1
+fi
+
+echo "Запуск QEMU с $KERNEL ..."
+$QEMU -machine $MACHINE -kernel $KERNEL $GRAPHIC -s -S -monitor none -serial mon:stdio

27c34fc030a47bfee4d5af3cfbd5f264b99a0229
scarletshroud <ezinovichev@gmail.com>
2025-04-16 00:09:40 +0300

    add impl of assertion mechanism

---
+++ tools/reliability_mechanisms/error_detection/improved_assertion.c
@@ -0,0 +1,24 @@
+#include "improved_assertion.h"
+
+#include <stdlib.h>
+
+assert_op_status_t reliability_assert_handler(const char* expr_str, const char* file, int line, assert_level_t level) {
+    const char* level_str = (level == ASSERT_LEVEL_WARNING) ? "WARNING" : (level == ASSERT_LEVEL_ERROR) ? "ERROR" : "CRITICAL";
+
+    // todo: to IO
+    //fprintf(stderr, "[%s] Assertion failed: (%s), file: %s, line: %d\n", level_str, expr_str, file, line);
+
+    // Дальнейшее поведение в зависимости от уровня
+    switch (level) {
+        case ASSERT_LEVEL_WARNING:
+            // Можно просто логировать
+            break;
+        case ASSERT_LEVEL_ERROR:
+            // Можно логировать и вернуться
+            break;
+        case ASSERT_LEVEL_CRITICAL:
+            // Завершаем выполнение
+            exit(1);
+            break;
+    }
+}

336678009f61c32c83f7509f18e268a4c5592c69
scarletshroud <ezinovichev@gmail.com>
2025-04-16 23:57:49 +0300

    upd snippets

---
+++ snippets/assert_snippet.h
@@ -0,0 +1,15 @@
+#ifndef ASSERT_SNIPPET_H_
+#define ASSERT_SNIPPET_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void no_assert_snippet(void);
+void assert_snippet(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

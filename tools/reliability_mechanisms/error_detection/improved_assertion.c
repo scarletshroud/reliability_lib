6bceef8b3c080cb01657f1052458f01b7f74eefd
scarletshroud <ezinovichev@gmail.com>
2025-04-16 19:08:50 +0300

    move assertion implementation

--- tools/reliability_mechanisms/error_detection/improved_assertion.c
+++ tools/reliability_mechanisms/error_detection/improved_assertion.c
@@ -2,23 +2,53 @@
 
 #include <stdlib.h>
 
+#include "error_monitor/error_monitor.h"
+
+// todo: https://erdani.org/publications/cuj-08-2003.php.html полезный топик
+
+// todo: добавить значения, которые использовались в expression для дополнения контекста
+// todo: интеграция с IO
+// todo: опция ignore
+// todo: интеграция с release mode
+
+static uint32_t assert_trigger_cnt = 0;
+
 assert_op_status_t reliability_assert_handler(const char* expr_str, const char* file, int line, assert_level_t level) {
     const char* level_str = (level == ASSERT_LEVEL_WARNING) ? "WARNING" : (level == ASSERT_LEVEL_ERROR) ? "ERROR" : "CRITICAL";
 
-    // todo: to IO
     //fprintf(stderr, "[%s] Assertion failed: (%s), file: %s, line: %d\n", level_str, expr_str, file, line);
+
+    assert_trigger_cnt++;
 
     // Дальнейшее поведение в зависимости от уровня
     switch (level) {
+        case ASSERT_LEVEL_DEBUG:
+            // ask the user what to do (Ignore/Debug/etc.)
+            break;
+
         case ASSERT_LEVEL_WARNING:
-            // Можно просто логировать
+            // Можно просто логировать и продолжить исполнение
             break;
+
         case ASSERT_LEVEL_ERROR:
-            // Можно логировать и вернуться
+            // Можно логировать и вернуться, выбросить исключение
+            error_monitor_save_event(file, "c", "assertion failed", line, ERROR_LEVEL_ERROR); 
             break;
+
         case ASSERT_LEVEL_CRITICAL:
             // Завершаем выполнение
+
+            // todo: Возможность перехода в безопасный режим
+            // todo: __builtin_trap(); остановка под отладчиком
+            // todo: поддержка пользовательского callback
+            // todo: возможное создание дампа состояния
+
+            error_monitor_save_event(file, "c", "assertion failed", line, ERROR_LEVEL_CRITICAL); 
+            
             exit(1);
+            break;
+
+        default:
             break;
     }
 }
--- tools/reliability_mechanisms/error_detection/improved_assertion.h
+++ tools/reliability_mechanisms/error_detection/improved_assertion.h
@@ -1,16 +1,17 @@
 #ifndef IMPROVED_ASSERTION_H_
 #define IMPROVED_ASSERTION_H_
 
-#include "../../tools_config.h"
+#include "tools_config.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef enum {
-    ASSERT_LEVEL_WARNING,
-    ASSERT_LEVEL_ERROR,
-    ASSERT_LEVEL_CRITICAL
+    ASSERT_LEVEL_DEBUG, // (it's the default, the usual assert)
+    ASSERT_LEVEL_WARNING, // (it's just a warning, the program can continue without user intervention)
+    ASSERT_LEVEL_ERROR, // an error
+    ASSERT_LEVEL_CRITICAL // (it's fatal, it's most likely the program/system got unstable).
 } assert_level_t;
 
 typedef enum {

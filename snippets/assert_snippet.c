c4767ecd3536b5459b6477574194940fdc20ea9a
scarletshroud <ezinovichev@gmail.com>
2025-04-17 11:48:34 +0300

    add assert

--- snippets/assert_snippet.c
+++ snippets/assert_snippet.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 #include "error_monitor/error_monitor.h"
+#include "reliability_mechanisms/error_detection/improved_assertion.h"
 
 #define RAND_MAX_CASE 5
 
@@ -19,9 +20,7 @@ void assert_snippet(void) {
     uint8_t system_error_case = 0;
     system_error_case = rand() % RAND_MAX_CASE;
 
-    if (system_error_case == 0) {
+    ASSERT(system_error_case != 0, ERROR_LEVEL_ERROR);
 
-    }
-    
-    error_monitor_save_event(__FILE__, "no_assert_snippet", "error", __LINE__, ERROR_LEVEL_ERROR);
+    error_monitor_save_event(__FILE__, "assert_snippet", "error", __LINE__, ERROR_LEVEL_ERROR);
 }

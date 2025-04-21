645cb79133c062de7e21e87edc4e8c1f49ed882c
scarletshroud <283916@niuitmo.ru>
2025-04-21 13:59:52 +0300

    formatting

--- platform/qemu/qemu_riscv_uart.h
+++ platform/qemu/qemu_riscv_uart.h
@@ -8,12 +8,12 @@
 #define UART0_DR    REG(UART0_BASE_ADDR, 0x00)
 #define UART0_FCR   REG(UART0_BASE_ADDR, 0x02)
 #define UART0_LSR   REG(UART0_BASE_ADDR, 0x05)
-																						
+
 #define UARTFCR_FFENA 0x01 // FIFO Control Register enable bit
 #define UARTLSR_THRE 0x20 // Line Status Register Transmit Hold Register Empty bit
 #define UART0_FF_THR_EMPTY (UART0_LSR & UARTLSR_THRE)
 
 void qemu_riscv_uart_enable(void);
-void qemu_riscv_uart_puts(const char *str);
+void qemu_riscv_uart_puts(const char* str);
 
 #endif

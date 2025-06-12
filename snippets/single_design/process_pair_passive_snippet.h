872c2bc4084c829fa9036455c03e8d4973a0c3bb
scarletshroud <ezinovichev@gmail.com>
2025-06-12 16:26:26 +0300

    add process pair passive snippet impl

---
+++ snippets/single_design/process_pair_passive_snippet.h
@@ -0,0 +1,14 @@
+#ifndef PROCESS_PAIR_PASSIVE_SNIPPET_H_
+#define PROCESS_PAIR_PASSIVE_SNIPPET_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    void process_pair_passive_snippet(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

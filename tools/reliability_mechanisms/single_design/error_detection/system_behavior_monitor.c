41523526801754c61975b0b5f77d5442c3066c00
scarletshroud <ezinovichev@gmail.com>
2025-06-14 14:08:29 +0300

    docs & refactoring of system monitor

--- tools/reliability_mechanisms/single_design/error_detection/system_behavior_monitor.c
+++ tools/reliability_mechanisms/single_design/error_detection/system_behavior_monitor.c
@@ -1,22 +1,33 @@
 #include "system_behavior_monitor.h"
 #include "error_monitor/error_monitor.h"      // Для фиксации ошибок
 
+#include "platform/adapter/platform_adapter.h"
+
 void behavior_monitor_init(behavior_monitor_t* monitor, uint32_t timeout_us, const char* context) {
     monitor->last_kick_timestamp_us = platform_timer_get_us();
     monitor->timeout_us = timeout_us;
     monitor->monitored_context = context;
-    monitor->active = true;
+    monitor->active = 0;
+
+    platform_wdt_init(timeout_us);
+}
+
+void behavior_monitor_start(behavior_monitor_t* monitor) {
+    platform_wdt_start();
+
+    monitor->active = 1;
 }
 
 void behavior_monitor_kick(behavior_monitor_t* monitor) {
     if (monitor && monitor->active) {
         monitor->last_kick_timestamp_us = platform_timer_get_us();
+        platform_wdt_kick();
     }
 }
 
-void behavior_monitor_check(const behavior_monitor_t* monitor) {
+uint8_t behavior_monitor_check(const behavior_monitor_t* monitor) {
     if (!monitor || !monitor->active) {
-        return;
+        return 0;
     }
 
     uint32_t now = platform_timer_get_us();
@@ -30,5 +41,8 @@ void behavior_monitor_check(const behavior_monitor_t* monitor) {
             __LINE__,
             ERROR_LEVEL_CRITICAL
         );
+        return 0;
     }
+
+    return 1;
 }
--- tools/reliability_mechanisms/single_design/error_detection/system_behavior_monitor.h
+++ tools/reliability_mechanisms/single_design/error_detection/system_behavior_monitor.h
@@ -2,27 +2,44 @@
 #define SYSTEM_BEHAVIOR_MONITOR_H
 
 #include <stdint.h>
-#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
     typedef struct {
-        uint32_t last_kick_timestamp_us;
-        uint32_t timeout_us;
-        const char* monitored_context;
-        bool active;
+        uint32_t last_kick_timestamp_us;    /* Последнее обновление сторожевого таймера */
+        uint32_t timeout_us;                /* Установленный таймаут */
+        const char* monitored_context;      /* Контекст мониторинга */
+        uint8_t active;                     /* Признак активности WDT */
     } behavior_monitor_t;
 
-    /// Инициализация монитора поведения
+    /**
+     * \brief           Функция инициализация монитора поведения
+     * \param[in]       monitor: Указатель на структуру монитора
+     * \param[in]       timeout_us: Таймаут
+     * \param[in]       context: Контекст монитора
+    */
     void behavior_monitor_init(behavior_monitor_t* monitor, uint32_t timeout_us, const char* context);
 
-    /// Подача признака активности (kick)
+    /**
+     * \brief           Функция запуска монитора поведения
+     * \param[in]       monitor: Указатель на структуру монитора
+    */
+    void behavior_monitor_start(behavior_monitor_t* monitor);
+ 
+    /**
+     * \brief           Функция подачи признака активности (kick)
+     * \param[in]       monitor: Указатель на структуру монитора
+    */
     void behavior_monitor_kick(behavior_monitor_t* monitor);
 
     /// Проверка срабатывания таймаута
-    void behavior_monitor_check(const behavior_monitor_t* monitor);
+    /**
+     * \brief           Функция проверки срабатывания таймаута
+     * \param[in]       monitor: Указатель на структуру монитора
+    */
+    uint8_t behavior_monitor_check(const behavior_monitor_t* monitor);
 
 #ifdef __cplusplus
 }

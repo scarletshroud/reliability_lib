14191991b0317b886d2f2537616bc36e97de9a17
scarletshroud <ezinovichev@gmail.com>
2025-06-16 13:09:55 +0300

    update main

--- main.c
+++ main.c
@@ -1,19 +1,19 @@
 #include <stdint.h>
 
-#include "snippets/assert_snippet.h"
+#include "snippets/snippets_suit.h"
+
 #include "tools/error_monitor/error_monitor.h"
 #include "tools/reliability_evaluation_models/discrete/shuman_based.h"
 
 #include "platform/adapter/platform_adapter.h"
 #include "platform/mk32_baremetal/src/mk32_system.h"
 
-
 int main() {
     /* System init */
     platform_init();
     platform_print("System was successfully initialized.\n");
 
+    start_snippets_suit();
     
-
     return 0;
 }

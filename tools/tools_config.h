6d00207f5ea07ef658b4cac11a049385d6a9b539
scarletshroud <ezinovichev@gmail.com>
2025-06-16 13:08:53 +0300

    add define to enable error monitor

--- tools/tools_config.h
+++ tools/tools_config.h
@@ -5,7 +5,11 @@
 extern "C" {
 #endif
 
+/* Включение ASSERTION для режима отладки */
 #define ENABLE_ASSERTS 1
+
+/* Включение Error Monitor */
+#define ERROR_MONITOR_ENABLE 1
 
 #ifdef __cplusplus
 }
